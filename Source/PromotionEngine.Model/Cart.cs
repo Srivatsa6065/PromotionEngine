32a57c0171fbe926cee32910ca8cb997ec74ae4e
Srivatsa Prasad <Srivatsa.Prasad@readynetworks.com>
2021-06-01 22:50:26 +0530

    Created model

---
+++ Source/PromotionEngine.Model/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine.Model
+{
+    public class Cart
+    {
+        public List<CartItem> CartItems { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
